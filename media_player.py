52741c459dee22ec4a2acd34b9c8b46cb4b7a319
Heatvent <matteocp@gmail.com>
2024-07-26 13:00:16 -0500

    Update media_player.py

--- media_player.py
+++ media_player.py
@@ -1,65 +1,26 @@
+import logging
 from homeassistant.components.media_player import MediaPlayerEntity
 from homeassistant.components.media_player.const import (
-    SUPPORT_TURN_ON, SUPPORT_TURN_OFF, SUPPORT_VOLUME_SET, SUPPORT_VOLUME_MUTE
+    SUPPORT_TURN_ON,
+    SUPPORT_TURN_OFF,
+    SUPPORT_VOLUME_SET,
 )
+from homeassistant.const import STATE_OFF, STATE_ON
 from .const import DOMAIN
-import logging
 
 _LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass, entry, async_add_entities):
-    _LOGGER.debug("Setting up media player entry: %s", entry.data)
-    config = hass.data[DOMAIN][entry.entry_id]
-    amplifier = Control4Amp(config["name"], config["host"], config["port"])
-    async_add_entities([amplifier] + amplifier.zones, True)
-    _LOGGER.debug("Added amplifier and zones to entities")
+    amplifier = hass.data[DOMAIN][entry.entry_id]
+    entities = [Control4AMPMediaPlayer(amplifier)]
+    async_add_entities(entities)
 
-class Control4Amp(MediaPlayerEntity):
-    def __init__(self, name, host, port):
-        _LOGGER.debug("Initializing Control4Amp: %s, %s, %s", name, host, port)
-        self._name = name
-        self._host = host
-        self._port = port
-        self._state = None
-        self.zones = [Control4Zone(self, zone_id) for zone_id in range(4)]
-
-    @property
-    def name(self):
-        return self._name
-
-    @property
-    def state(self):
-        return self._state
-
-    def turn_on(self):
-        # Implement turn on logic
-        _LOGGER.debug("Turning on amplifier: %s", self._name)
-        self._state = "on"
-        self.schedule_update_ha_state()
-
-    def turn_off(self):
-        # Implement turn off logic
-        _LOGGER.debug("Turning off amplifier: %s", self._name)
-        self._state = "off"
-        self.schedule_update_ha_state()
-
-    def set_volume_level(self, volume):
-        # Implement set volume logic
-        _LOGGER.debug("Setting volume level for amplifier: %s to %s", self._name, volume)
-        pass
-
-    def mute_volume(self, mute):
-        # Implement mute logic
-        _LOGGER.debug("Muting volume for amplifier: %s to %s", self._name, mute)
-        pass
-
-class Control4Zone(MediaPlayerEntity):
-    def __init__(self, amplifier, zone_id):
-        _LOGGER.debug("Initializing Control4Zone: %s Zone %d", amplifier.name, zone_id)
+class Control4AMPMediaPlayer(MediaPlayerEntity):
+    def __init__(self, amplifier):
         self._amplifier = amplifier
-        self._zone_id = zone_id
-        self._name = f"{amplifier.name} Zone {zone_id + 1}"
-        self._state = None
+        self._name = "Control4 Amplifier"
+        self._state = STATE_OFF
+        self._volume = 0
 
     @property
     def name(self):
@@ -69,24 +30,25 @@ class Control4Zone(MediaPlayerEntity):
     def state(self):
         return self._state
 
-    def turn_on(self):
-        # Implement zone turn on logic
-        _LOGGER.debug("Turning on zone: %s", self._name)
-        self._state = "on"
-        self.schedule_update_ha_state()
+    @property
+    def supported_features(self):
+        return SUPPORT_TURN_ON | SUPPORT_TURN_OFF | SUPPORT_VOLUME_SET
 
-    def turn_off(self):
-        # Implement zone turn off logic
-        _LOGGER.debug("Turning off zone: %s", self._name)
-        self._state = "off"
-        self.schedule_update_ha_state()
+    @property
+    def volume_level(self):
+        return self._volume
 
-    def set_volume_level(self, volume):
-        # Implement set volume logic
-        _LOGGER.debug("Setting volume level for zone: %s to %s", self._name, volume)
-        pass
+    async def async_turn_on(self):
+        await self._amplifier.send_command("turn_on")
+        self._state = STATE_ON
+        self.async_write_ha_state()
 
-    def mute_volume(self, mute):
-        # Implement mute logic
-        _LOGGER.debug("Muting volume for zone: %s to %s", self._name, mute)
-        pass
+    async def async_turn_off(self):
+        await self._amplifier.send_command("turn_off")
+        self._state = STATE_OFF
+        self.async_write_ha_state()
+
+    async def async_set_volume_level(self, volume):
+        await self._amplifier.send_command(f"set_volume {volume}")
+        self._volume = volume
+        self.async_write_ha_state()
