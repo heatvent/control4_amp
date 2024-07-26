8dbdd4a4fd9770407bd04447adc0e3ef529752f5
Heatvent <matteocp@gmail.com>
2024-07-26 13:01:30 -0500

    Update config_flow.py

--- config_flow.py
+++ config_flow.py
@@ -1,22 +1,32 @@
-from homeassistant import config_entries
 import voluptuous as vol
+from homeassistant import config_entries
+from homeassistant.core import callback
 from .const import DOMAIN
 
-class Control4AmpConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+@callback
+def configured_instances(hass):
+    return set(
+        entry.data["ip_address"] for entry in hass.config_entries.async_entries(DOMAIN)
+    )
+
+class Control4AMPConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
-    def __init__(self):
-        pass
-
     async def async_step_user(self, user_input=None):
+        errors = {}
         if user_input is not None:
-            return self.async_create_entry(title=user_input["name"], data=user_input)
+            if user_input["ip_address"] in configured_instances(self.hass):
+                errors["base"] = "ip_exists"
+            else:
+                return self.async_create_entry(title="Control4 AMP", data=user_input)
+
+        data_schema = vol.Schema(
+            {
+                vol.Required("ip_address"): str,
+                vol.Required("port", default=8750): int,
+            }
+        )
 
         return self.async_show_form(
-            step_id="user",
-            data_schema=vol.Schema({
-                vol.Required("name"): str,
-                vol.Required("host"): str,
-                vol.Required("port"): int,
-            })
+            step_id="user", data_schema=data_schema, errors=errors
         )
