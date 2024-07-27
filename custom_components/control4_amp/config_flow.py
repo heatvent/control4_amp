f1c5b6524aef99caeb24638ec0808578a2a2e982
Heatvent <matteocp@gmail.com>
2024-07-27 14:47:56 -0500

    Update config_flow.py

--- custom_components/control4_amp/config_flow.py
+++ custom_components/control4_amp/config_flow.py
@@ -1,7 +1,7 @@
 import logging
 import ipaddress
 import voluptuous as vol
-from homeassistant import config_entries, exceptions
+from homeassistant import config_entries
 from homeassistant.core import callback
 from homeassistant.const import CONF_IP_ADDRESS, CONF_PORT, CONF_NAME
 from .const import DOMAIN
@@ -14,11 +14,6 @@ class Control4AmpConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
     CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_PUSH
 
-    @staticmethod
-    @callback
-    def async_get_options_flow(config_entry):
-        return OptionsFlow(config_entry)
-
     async def async_step_user(self, user_input=None):
         """Handle a flow initialized by the user interface."""
         errors = {}
@@ -28,7 +23,10 @@ class Control4AmpConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 if user_input[CONF_PORT] <= 0 or user_input[CONF_PORT] > 65535:
                     raise ValueError("Invalid port number.")
                 _LOGGER.debug("Processing user input: %s", user_input)
-                return self.async_create_entry(title="Control4 Amp", data=user_input)
+                # Unique ID can be set as IP or another unique identifier
+                await self.async_set_unique_id(user_input[CONF_IP_ADDRESS])
+                self._abort_if_unique_id_configured()
+                return self.async_create_entry(title=user_input[CONF_NAME], data=user_input)
             except ValueError as e:
                 _LOGGER.error("Validation error: %s", e)
                 errors["base"] = "invalid_input"
@@ -41,30 +39,4 @@ class Control4AmpConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 vol.Required(CONF_PORT, default=8750): int,
             }),
             errors=errors
-        )
-
-class InvalidInput(exceptions.HomeAssistantError):
-    """Error to indicate the input is invalid."""
-
-class OptionsFlow(config_entries.OptionsFlow):
-    """Handle an options flow for Control4 Amp."""
-
-    def __init__(self, config_entry):
-        self.config_entry = config_entry
-
-    async def async_step_init(self, user_input=None):
-        """Manage the options."""
-        return await self.async_step_user()
-
-    async def async_step_user(self, user_input=None):
-        """Handle a flow initialized by the user."""
-        if user_input is not None:
-            return self.async_create_entry(title="", data=user_input)
-
-        return self.async_show_form(
-            step_id="user",
-            data_schema=vol.Schema({
-                vol.Optional(CONF_IP_ADDRESS, default=self.config_entry.options.get(CONF_IP_ADDRESS)): str,
-                vol.Optional(CONF_PORT, default=self.config_entry.options.get(CONF_PORT)): int,
-            })
         )
