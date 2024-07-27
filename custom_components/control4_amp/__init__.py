9ae9295ef5dc31c237a53301e2ea278dc3bfe9bf
Heatvent <matteocp@gmail.com>
2024-07-27 12:18:43 -0500

    Update __init__.py

--- custom_components/control4_amp/__init__.py
+++ custom_components/control4_amp/__init__.py
@@ -1,33 +1,24 @@
-import logging
-from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.helpers.typing import HomeAssistantType
-
+from homeassistant.core import HomeAssistant
 from .const import DOMAIN
+from .media_player import setup_platform as setup_media_player
 from .udp_communication import UDPCommunication
 
-_LOGGER = logging.getLogger(__name__)
-
-async def async_setup(hass: HomeAssistant, config):
-    """Set up configured Control4 Amp."""
-    return True
-
-async def async_setup_entry(hass: HomeAssistantType, entry: ConfigEntry):
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up Control4 Amp from a config entry."""
     hass.data.setdefault(DOMAIN, {})
-    udp_comm = UDPCommunication(entry.data["ip_address"], entry.data["port"])
+
+    udp_comm = UDPCommunication(entry.data['ip_address'], entry.data['port'])
     await udp_comm.start()
+
     hass.data[DOMAIN][entry.entry_id] = udp_comm
 
-    # Load platforms
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setups(entry, ["media_player"])
-    )
+    await setup_media_player(hass, entry, udp_comm)
+
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Unload a config entry."""
-    unload_ok = await hass.config_entries.async_unload_platforms(entry, ["media_player"])
-    if unload_ok:
-        hass.data[DOMAIN].pop(entry.entry_id)
-    return unload_ok
+    udp_comm = hass.data[DOMAIN].pop(entry.entry_id)
+    await udp_comm.stop()
+    return True
