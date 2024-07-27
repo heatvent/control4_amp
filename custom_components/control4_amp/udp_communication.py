5fa17ea1cf1cffb3338aa2ae241ca88dd16b265f
Heatvent <matteocp@gmail.com>
2024-07-27 12:23:35 -0500

    Update udp_communication.py

--- custom_components/control4_amp/udp_communication.py
+++ custom_components/control4_amp/udp_communication.py
@@ -4,43 +4,56 @@ import logging
 _LOGGER = logging.getLogger(__name__)
 
 class UDPCommunication:
-    """UDP Communication handler for Control4 Amp."""
+    """Class to handle UDP communication with Control4 Amp."""
 
     def __init__(self, ip, port):
         self.ip = ip
         self.port = port
+        self.transport = None
         self.loop = asyncio.get_event_loop()
+        self.firmware_callback = None  # Ensure there's a default callback
+
+    def connection_made(self, transport):
+        """Called when the connection is made."""
+        self.transport = transport
+        _LOGGER.info('Connection made to %s:%s', self.ip, self.port)
+
+    def datagram_received(self, data, addr):
+        """Handle received data."""
+        message = data.decode()
+        _LOGGER.info('Received message: %s from %s', message, addr)
+        # Process the received data here, such as extracting firmware version
+
+    def error_received(self, exc):
+        """Handle received error."""
+        _LOGGER.error('Error received: %s', exc)
+
+    def connection_lost(self, exc):
+        """Handle connection lost."""
+        _LOGGER.warning('Connection lost: %s', exc)
         self.transport = None
 
     async def start(self):
-        """Start the UDP server."""
+        """Start UDP server."""
         self.transport, _ = await self.loop.create_datagram_endpoint(
             lambda: self,
             remote_addr=(self.ip, self.port)
         )
-        _LOGGER.info("UDP server started on %s:%s", self.ip, self.port)
-
-    def send_data(self, data):
-        """Send data to the UDP endpoint."""
-        self.transport.sendto(data.encode(), (self.ip, self.port))
-        _LOGGER.info("Sent data: %s", data)
-
-    def request_firmware_version(self, callback):
-        """Request firmware version from the amp."""
-        self.send_data("0gha00 c4.sy.fwv")
-        self.firmware_callback = callback
-
-    def datagram_received(self, data, addr):
-        """Handle received datagrams."""
-        message = data.decode()
-        _LOGGER.info("Received message: %s", message)
-        if 'c4.sy.fwv' in message:
-            version = message.split('"')[1]
-            if self.firmware_callback:
-                self.firmware_callback(version)
+        _LOGGER.info('UDP server started on %s:%s', self.ip, self.port)
 
     async def stop(self):
-        """Stop the UDP server."""
+        """Stop UDP server."""
         if self.transport:
             self.transport.close()
-            _LOGGER.info("UDP server stopped.")
+            _LOGGER.info('UDP connection closed')
+
+    def send_data(self, data):
+        """Send data via UDP."""
+        if self.transport:
+            self.transport.sendto(data.encode(), (self.ip, self.port))
+            _LOGGER.info('Sent data to %s:%s', self.ip, self.port)
+
+    def request_firmware_version(self):
+        """Request firmware version from the amp."""
+        command = "0gha00 c4.sy.fwv"
+        self.send_data(command)
