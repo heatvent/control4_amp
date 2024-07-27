306ce9af75486fa732dd9c8583e3c07d63836c8d
Heatvent <matteocp@gmail.com>
2024-07-27 14:31:29 -0500

    Update udp_communication.py

--- custom_components/control4_amp/udp_communication.py
+++ custom_components/control4_amp/udp_communication.py
@@ -1,55 +1,68 @@
 import asyncio
 import logging
+from homeassistant.helpers.dispatcher import async_dispatcher_send
 
 _LOGGER = logging.getLogger(__name__)
 
 class UDPCommunication:
     """Class to handle UDP communication with Control4 Amp."""
 
-    def __init__(self, ip, port):
+    def __init__(self, hass, ip, port):
+        self.hass = hass
         self.ip = ip
         self.port = port
         self.transport = None
-        self.loop = asyncio.get_event_loop()
+        self.loop = asyncio.get_running_loop()
         self.sequence = 0
 
     def connection_made(self, transport):
+        """Handle connection made event."""
         self.transport = transport
-        _LOGGER.info('Connection made to %s:%s', self.ip, self.port)
+        _LOGGER.info("Connection made with %s:%s", self.ip, self.port)
 
     def datagram_received(self, data, addr):
+        """Handle incoming UDP data."""
         message = data.decode()
-        _LOGGER.info('Received message: %s from %s', message, addr)
-        # Process response here
-        if "c4.sy.fwv" in message:
-            version = message.split('"')[1]
-            _LOGGER.info('Firmware version received: %s', version)
-            # Update Home Assistant state here
+        _LOGGER.info("Received from %s: %s", addr, message)
+        self.handle_response(message)
 
     def error_received(self, exc):
-        _LOGGER.error('Error received: %s', exc)
+        """Handle errors."""
+        _LOGGER.error("UDP error received: %s", exc)
 
     def connection_lost(self, exc):
-        _LOGGER.warning('Connection lost: %s', exc)
+        """Handle connection lost."""
+        _LOGGER.warning("UDP connection lost: %s", exc)
         self.transport = None
 
     async def start(self):
+        """Start UDP server."""
         self.transport, _ = await self.loop.create_datagram_endpoint(
             lambda: self,
             remote_addr=(self.ip, self.port)
         )
 
     async def stop(self):
+        """Stop UDP server."""
         if self.transport:
             self.transport.close()
-            _LOGGER.info('UDP connection closed')
+            _LOGGER.info("UDP connection closed")
 
     def send_data(self, command):
+        """Send data to Control4 Amp."""
         data = f'0g{self.sequence:02d}ha00 {command}\r\n'
         if self.transport:
             self.transport.sendto(data.encode(), (self.ip, self.port))
-            _LOGGER.info('Sent command: %s to %s:%s', data, self.ip, self.port)
-        self.sequence = (self.sequence + 1) % 100  # wrap around at 99
+            _LOGGER.info("Sent: %s", data)
+        self.sequence = (self.sequence + 1) % 100  # Wrap around at 99
+
+    def handle_response(self, message):
+        """Handle responses from the amplifier."""
+        if "c4.sy.fwv" in message:
+            version = message.split('"')[1]
+            _LOGGER.info("Firmware version: %s", version)
+            async_dispatcher_send(self.hass, f"{DOMAIN}_update_firmware", version)
 
     def request_firmware_version(self):
+        """Send command to request firmware version."""
         self.send_data("c4.sy.fwv")
