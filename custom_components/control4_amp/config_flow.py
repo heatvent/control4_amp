fe8a2153ddfe06f39318ee2641e532c34b1091fa
Heatvent <matteocp@gmail.com>
2024-07-27 09:32:03 -0500

    Update config_flow.py

--- custom_components/control4_amp/config_flow.py
+++ custom_components/control4_amp/config_flow.py
@@ -1,26 +1,27 @@
+import logging
 import voluptuous as vol
 from homeassistant import config_entries
-from homeassistant.core import callback
 from .const import DOMAIN
 
-@callback
-def configured_instances(hass):
-    """Return a set of configured Control4 AMP instances."""
-    return set(entry.data["host"] for entry in hass.config_entries.async_entries(DOMAIN))
+_LOGGER = logging.getLogger(__name__)
 
-class Control4AMPConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    VERSION = 1
+class Control4AmpConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Manage the Control4 Amp configuration flow."""
 
     async def async_step_user(self, user_input=None):
+        """Handle a flow initialized by the user interface."""
+        _LOGGER.debug("User step called with input: %s", user_input)
+        errors = {}
         if user_input is not None:
-            return self.async_create_entry(title=user_input["name"], data=user_input)
-
-        data_schema = vol.Schema({
-            vol.Required("name"): str,
-            vol.Required("host"): str,
-            vol.Required("port"): int,
-        })
+            _LOGGER.debug("Processing user input: %s", user_input)
+            return self.async_create_entry(title="Control4 Amp", data=user_input)
 
         return self.async_show_form(
-            step_id="user", data_schema=data_schema
+            step_id="user",
+            data_schema=vol.Schema({
+                vol.Required("name"): str,
+                vol.Required("ip_address"): str,
+                vol.Required("port", default=8750): int,
+            }),
+            errors=errors
         )
