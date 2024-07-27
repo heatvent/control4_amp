37594d86d0ebbe7f482683951dbc12610a105e9b
Heatvent <matteocp@gmail.com>
2024-07-27 14:48:14 -0500

    Update __init__.py

--- custom_components/control4_amp/__init__.py
+++ custom_components/control4_amp/__init__.py
@@ -1,5 +1,6 @@
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.helpers import discovery
 from .const import DOMAIN
 
 async def async_setup(hass: HomeAssistant, config: dict):
@@ -8,11 +9,16 @@ async def async_setup(hass: HomeAssistant, config: dict):
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up Control4 Amp from a config entry."""
-    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = entry.data
+    # Store a connection instance in hass.data
+    if DOMAIN not in hass.data:
+        hass.data[DOMAIN] = {}
+    hass.data[DOMAIN][entry.entry_id] = entry.data
     await hass.config_entries.async_forward_entry_setups(entry, ["media_player"])
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Unload a config entry."""
-    await hass.config_entries.async_unload_platforms(entry, ["media_player"])
-    return True
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, ["media_player"])
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+    return unload_ok
